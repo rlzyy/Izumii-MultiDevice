dd97265130ba180cd10786c370e6cbb59ea2ab1e
Irull2nd <123540213+Izumii44@users.noreply.github.com>
2024-07-11 15:15:50 +0700

    Create ytdl.js

---
+++ plugins/download/ytdl.js
@@ -0,0 +1,40 @@
+import { ytmp4 } from "../../storage/script/yt-core.js";
+
+export default {
+    command: ["ytdl"],
+    description: "Mengunduh video youtube",
+    example: "Example: %p%cmd <Url>",
+    name: "ytdl",
+    tags: "download",
+
+    limit: true,
+
+    run: async(m, { conn, text }) => {
+    try {
+        let Ytdl = await ytmp4(m.text)
+        const video = Ytdl.url
+        const title = Ytdl.title
+        const duration = Ytdl.duration
+        const cenel = Ytdl.channel
+        const publish = Ytdl.published
+        const view = Ytdl.views
+        const cap = `
+         乂 *Y T  M P 4*
+    
+    ⚘ *Title* : ${title}
+    ⚘ *Channel* : ${cenel}
+    ⚘ *publish* : ${publish}
+    ⚘ *views* : ${view}
+    ⚘ *Resolusi* : 360p
+    ⚘ *Url* : ${text}
+        `
+
+        await conn.sendMessage(m.chat,  { video: { url: video }, caption: cap, mimetype: "video/mp4", fileName: title, contextInfo: { externalAdReply: { showAdAttribution: true, mediaType: 2} }
+        }, {
+            quoted: m
+        })
+    } catch (err) {
+      m.reply(`Error: ${err.message}`);
+    }
+  }
+}
