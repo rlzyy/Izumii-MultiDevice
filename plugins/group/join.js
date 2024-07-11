1dbd2b56f824be4735f16e64e0cc8f63bb220569
Irull2nd <123540213+Izumii44@users.noreply.github.com>
2024-07-11 15:20:50 +0700

    Create join.js

---
+++ plugins/group/join.js
@@ -0,0 +1,22 @@
+const isNumber = (x) => (x = parseInt(x), typeof x === 'number' && !isNaN(x))
+
+export default {
+    command: ["join"],
+    description: "Masuk dalam group",
+    example: "",
+    name: "join",
+    tags: "group",
+
+    owner: true,
+
+    run: async(m, { conn, text, isAdmin, isBotAdmin }) => {
+        try {
+    let [_, code] = text.match(/chat.whatsapp.com\/([0-9A-Za-z]{20,24})( [0-9]{1,3})?/i) || []
+    if (!code) return m.reply('Link invalid');
+    let res = await conn.groupAcceptInvite(code)
+    m.reply(`Successfully Joined Group`)
+} catch (e) {
+throw eror
+}
+    }
+}
