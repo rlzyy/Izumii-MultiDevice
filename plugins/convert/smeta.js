0c8282f39db53ab078003a1eabbbabd982aff616
Irull2nd <123540213+Izumii44@users.noreply.github.com>
2024-07-14 13:08:52 +0700

    Create smeta.js

---
+++ plugins/convert/smeta.js
@@ -0,0 +1,27 @@
+import { exifAvatar } from '../../system/lib/sticker.js';
+
+export default {
+	command: ["smeta"],
+    description: "Membuat stiker meta",
+    example: "",
+    name: "smeta",
+    tags: "convert",
+
+    run: async(m, { conn, command }) => {
+      var stiker = false
+      try {
+        let q = m.quoted ? m.quoted : m
+        let mime = (q.msg || q).mimetype || q.mediaType || ''
+        if (/webp/g.test(mime)) {
+            let img = await q.download()
+        var stiker = await exifAvatar(img, global.packname, global.wm)
+      }
+          } catch (e) {
+        console.error(e)
+        if (Buffer.isBuffer(e)) stiker = e
+      } finally {
+        if (stiker) conn.sendMessage(m.chat, { sticker: stiker }, { quoted: m })
+        else m.reply(`*Conversion failed*\nReply sticker with commands ${m.prefix + command}`)
+    }
+    }
+}; 
