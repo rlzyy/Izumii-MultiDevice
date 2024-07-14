ca7f067e7340514187b3eae71fd07facba0e5db3
Irull2nd <123540213+Izumii44@users.noreply.github.com>
2024-07-14 13:29:03 +0700

    Update instagram.js

--- plugins/download/instagram.js
+++ plugins/download/instagram.js
@@ -6,38 +6,34 @@ export default {
   tags: "download",
 
   run: async (m, { conn, args }) => {
-if (!/https?:\/\/(www\.)?instagram\.com/i.test(args[0]))
-
+if (!/(?:(?:http|https):\/\/)?(?:www\.)?(?:instagram\.com|instagr\.am)\/([A-Za-z0-9-_\.]+)/i.test(args[0]))
 		throw `Link Invalid!`;
-
     let response;
-    const apiUrl = API("fumi", "/api/instagram", { url: args[0] });
-
+    const apiUrl = API("itzpire", "/download/instagram", { url: args[0] });
     try {
       response = await func.fetchJson(apiUrl);
     } catch (err) {
       return m.reply("An error occurred while fetching the Instagram reel.");
     }
-
-    if (!response || response.status !== 200 || !response.data) {
+    if (!response || response.status !== "success" || !response.data) {
       return m.reply("Failed to fetch Instagram video/image.");
     }
-
-    
-    const videoUrl = response.data.response[0].url;
-
+    const videoUrl = response.data
     if (!videoUrl) {
       return m.reply("Video URL not found.");
     }
-
     try {
-    let start = new Date();
-  let sp = `${((new Date - start) * 1)}`
-  let cap = `🍟 *Fetching* ${sp} ms`
-      await m.reply(videoUrl, { caption: cap });
+      await conn.sendMessage(m.chat, {
+                    video: {
+                        url: videoUrl
+                    },
+                    caption: 'Successfully downloading Instagram videos'
+                }, {
+                    quoted: m
+                })
     } catch (err) {
       conn.logger.error(err);
-      m.reply("error");
+      m.reply("Error feature, please try again");
     }
   },
 };
