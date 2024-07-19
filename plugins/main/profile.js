23a6727a38ac970604476589c7a1ba8953f59389
Irull2nd <123540213+Izumii44@users.noreply.github.com>
2024-07-19 22:38:19 +0700

    Update profile.js

--- plugins/main/profile.js
+++ plugins/main/profile.js
@@ -1,6 +1,6 @@
 export default {
     command: ["profile", "my"],
-    description: "Melihat kartu profil mu",
+    description: "View your profile card",
     example: "",
     name: "profile",
     tags: "main",
@@ -9,7 +9,7 @@ export default {
 
     run: async (m, { conn }) => {
         let user = db.users[m.sender]
-        let tek = `[ *Your profile* ]\n\n*Name:* ${user.name}\n*Umur:* ${user.age}\n*Exp:* ${user.exp}\n*Level:* ${user.level}\n*Limit:* ${user.limit}\n*Money:* ${user.money}\n*Status:* ${user.premium ? "Premium" : "Free"}${user.premiumTime >= 1 ? "\n*Expired:* " + clockString(user.premiumTime - Date.now()) : ""}\n*Registrasi:* Ya\n*Waktu Registrasi:* ${func.jam(user.regTime, { timeZone: "Asia/Jakarta" })}, ${func.tanggal(user.regTime, "Asia/Jakarta")}`
+        let tek = `[ *Your profile* ]\n\n*Name:* ${user.name}\n*Age:* ${user.age}\n*Exp:* ${user.exp}\n*Level:* ${user.level}\n*Limit:* ${user.limit}\n*Money:* ${user.money}\n*Status:* ${user.premium ? "Premium" : "Free"}${user.premiumTime >= 1 ? "\n*Expired:* " + clockString(user.premiumTime - Date.now()) : ""}\n*Registration:* Yes\n*Registration Time:* ${func.jam(user.regTime, { timeZone: "Asia/Jakarta" })}, ${func.tanggal(user.regTime, "Asia/Jakarta")}`
     return conn.sendMessage(m.chat, { text: tek }, { quoted: m });
     }
 }
@@ -19,5 +19,5 @@ function clockString(ms) {
     let h = isNaN(ms) ? "" : Math.floor(ms / 3600000) % 24
     let m = isNaN(ms) ? "" : Math.floor(ms / 60000) % 60
     let s = isNaN(ms) ? "" : Math.floor(ms / 1000) % 60
-    return [d, " Hari ", h, " Jam ", m, " Menit ", s, " Detik"].map(v => v.toString().padStart(2, 0)).join("")
+    return [d, " Day ", h, " O'clock ", m, " Minute ", s, " Second"].map(v => v.toString().padStart(2, 0)).join("")
 }
