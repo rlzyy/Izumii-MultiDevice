e03a0c003b125dd68cf69de394ec3ff9f2fe0afe
Irull2nd <123540213+Izumii44@users.noreply.github.com>
2024-07-19 22:32:21 +0700

    Update link-group.js

--- plugins/group/link-group.js
+++ plugins/group/link-group.js
@@ -1,11 +1,12 @@
 export default {
     command: ["linkgroup", "linkgc", "linkgrup"],
-    description: "Mengambil link group dan membagikan ke member",
+    description: "Take the group link and share it with members",
     example: "",
     name: "linkgroup",
     tags: "group",
 
     botAdmin: true,
+    group: true,
 
     run: async(m, { conn }) => {
         m.reply("https://chat.whatsapp.com/" + await conn.groupInviteCode(m.chat))
