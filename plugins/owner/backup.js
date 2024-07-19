d5bd772dfafad006cbcd8957a836bb589b038ff3
Irull2nd <123540213+Izumii44@users.noreply.github.com>
2024-07-19 22:45:31 +0700

    Update backup.js

--- plugins/owner/backup.js
+++ plugins/owner/backup.js
@@ -3,7 +3,7 @@ import cp, { exec as _exec } from "child_process"
 
 export default {
     command: ["backup"],
-    description: "Mencadangkan file",
+    description: "Backing up files",
     example: "",
     name: "backup",
     tags: "owner",
@@ -15,7 +15,7 @@ export default {
         let exec = promisify(_exec).bind(cp)
         let { stdout } = await exec("zip -r storage/backup.zip * -x 'node_modules/*'")
 
-        if (stdout) conn.sendMessage(m.chat, { document: await func.fs.readFileSync("./storage/backup.zip"), fileName: "backup-script.zip", mimetype: "application/zip", caption: "Berhasil mencadangkan script [ ✅ ]" }, { quoted: m })
+        if (stdout) conn.sendMessage(m.chat, { document: await func.fs.readFileSync("./storage/backup.zip"), fileName: "backup-script.zip", mimetype: "application/zip", caption: "Successfully backed up the script [ ✅ ]" }, { quoted: m })
         func.fs.unlinkSync("./storage/backup.zip")
     }
 }
