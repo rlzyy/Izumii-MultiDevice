81e42bd0aba5459acd88698827e56e89f8a091a4
Irull2nd <123540213+Izumii44@users.noreply.github.com>
2024-07-19 22:58:20 +0700

    Update banknarik.js

--- plugins/rpg/banknarik.js
+++ plugins/rpg/banknarik.js
@@ -2,20 +2,20 @@ import fs from 'fs';
 
 export default {
     command: ["narik", "tarik"],
-    description: "Tarik uangmu di atm",
+    description: "Withdraw your money at the ATM",
     example: "",
-    name: "atm",
+    name: "tarik",
     tags: "rpg",
 
     run: async(m, { conn, command, args }) => {
 	let user = global.db.users[m.sender]
-	if (user.atm == 0) return m.reply(`[!] Anda belum memiliki ATM.\n\ngunakan command *${m.prefix}atm create* untuk memproses.`)
+	if (user.atm == 0) return m.reply(`[!] You don't have an ATM yet.\n\use command *${m.prefix}atm create* to process.`)
 	if (func.somematch(['all', 'semua'], args[0])) args[0] = user.atm - 50000
 	let total = Math.floor(func.isNumber(args[0]) ? Math.min(Math.max(parseInt(args[0]), 1), Number.MAX_SAFE_INTEGER) : 1) * 1
 	if ((user.atm - total) >= 50000) {
 		user.atm -= total
 		user.money += total
-		m.reply(`Sukses menarik sebesar ${total} Money 💹`)
-	} else m.reply(`[❗] Rekening anda tidak mencukupi untuk menarik ${total} money 💹`)
+		m.reply(`Success attracts as much as ${total} Money 💹`)
+	} else m.reply(`[❗] Your account is insufficient to withdraw ${total} money 💹`)
 }
 }
