930027d6aa29aa1eb4a14b52867b8b0d7e22c45e
Irull2nd <123540213+Izumii44@users.noreply.github.com>
2024-07-19 22:35:59 +0700

    Update donate.js

--- plugins/main/donate.js
+++ plugins/main/donate.js
@@ -1,6 +1,6 @@
 export default {
     command: ["donasi"],
-    description: "Bantu owner dengan donasi",
+    description: "Help the owner with donations",
     example: "",
     name: "donasi",
     tags: "main",
@@ -8,6 +8,6 @@ export default {
     register: true,
 
     run: async (m, { conn }) => {
-    await conn.sendMessage(m.chat, { image: { url: "https://pomf2.lain.la/f/ojyx0bp.jpg" }, caption: 'Bantu Developer Dengan Cara Berdonasi Agar Developer Semangat Buat Update' }, { quoted: m })
-        }
+    await conn.sendMessage(m.chat, { image: { url: "https://pomf2.lain.la/f/ojyx0bp.jpg" }, caption 'Help Developers by Donating to Encourage Developers to Make Updates' }, { quoted: m })
+    }
 }
