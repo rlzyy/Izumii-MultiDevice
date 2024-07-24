6aa1e1ef14ee5f005dac79720744f6e47e74f8c4
Irull2nd <123540213+Izumii44@users.noreply.github.com>
2024-07-24 17:15:57 +0700

    Update daily.js

--- plugins/rpg/daily.js
+++ plugins/rpg/daily.js
@@ -2,9 +2,8 @@ import fs from 'fs';
 const rewards = {
   exp: 12000,
   money: 10000,
-  potion: 5,
+  potion: 5
 }
-const cooldown = 86400000
 
 export default {
     command: ["claim", "daily"],
@@ -12,18 +11,26 @@ export default {
     example: "",
     name: "daily",
     tags: "rpg",
-    cooldown: cooldown,
-
+    
+    register: true,
+   
     run: async(m, { conn, command, args }) => {
 	let user = global.db.users[m.sender]
-  if (new Date - user.lastclaim < cooldown) throw `You have already claimed this daily claim!, wait for a few hours`
+	let _timers = 86400000 - (new Date() - user.lastclaim);
+    let timers = await func.clockString(_timers);
+    if (new Date() - user.lastclaim > 86400000) {
   let text = ''
   for (let reward of Object.keys(rewards)) {
 	if (!(reward in user)) continue
 	user[reward] += rewards[reward]
-	text += `*+${rewards[reward]}* ✨ ${(reward)}${reward}\n`
+	text += `*+${rewards[reward]}* ✨ ${(reward)}\n`
   }
   m.reply(text.trim())
-  user.lastclaim = new Date * 1
-}
+  user.lastclaim = new Date() * 1;
+   } else {
+      m.reply(
+        `🚩 You have made a claim today, please wait until tomorrow to claim again.\n\nTimeout : [ *${timers}* ]`,
+      );
+     }
+ }
 }
