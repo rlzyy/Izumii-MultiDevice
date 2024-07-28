0f0063925dd44bdd8adc6a937fc977305b9c92a6
Irull2nd <123540213+Izumii44@users.noreply.github.com>
2024-07-28 13:30:25 +0700

    Update donate.js

--- plugins/main/donate.js
+++ plugins/main/donate.js
@@ -8,6 +8,6 @@ export default {
     register: true,
 
     run: async (m, { conn }) => {
-    await conn.sendMessage(m.chat, { image: { url: "https://pomf2.lain.la/f/ojyx0bp.jpg" }, caption 'Help Developers by Donating to Encourage Developers to Make Updates' }, { quoted: m })
+    await conn.sendMessage(m.chat, { image: { url: "https://pomf2.lain.la/f/ojyx0bp.jpg" }, caption: 'Help Developers by Donating to Encourage Developers to Make Updates' }, { quoted: m })
     }
 }
