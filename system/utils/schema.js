4555a0ef1231775e9328ab49a4fffe646f0851fe
Irull2nd <123540213+Izumii44@users.noreply.github.com>
2024-07-28 13:36:31 +0700

    take session data and organize it in the mongodb database

---
+++ system/utils/schema.js
@@ -0,0 +1,25 @@
+import "../../storage/config.js"
+import { MongoClient } from "mongodb";
+import mongoose from "mongoose";
+import chalk from "chalk";
+
+export const connectAuth = async() => {
+  if (global.mongoAuth) {
+    console.log(chalk.blue('Connecting To MongoDB Server...'));
+  }
+if (!global.mongoURL) {
+    console.warn("Warning: No MongoDB link available.");
+} else {
+    global.client = new MongoClient(global.mongoURL);
+    global.client.connect(err => {
+        if (err) {
+            console.warn("Warning: MongoDB link is invalid or cannot be connected.");
+        } else {
+            console.log(chalk.green('Successfully Connected To MongoDB Server'))
+        }
+    });
+}
+  await client.connect();
+  const collection = client.db("irull2nd").collection("sessions")
+  return collection;
+};
